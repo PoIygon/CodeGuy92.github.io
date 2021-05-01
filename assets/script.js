2f4124f921e21960881d126fe85c7b24d6aeafee
LankyBox01 <79767244+CodeGuy92@users.noreply.github.com>
2021-05-01 16:43:26 +0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,6 @@
-console.log("Hi!")
+function onload() {
+  var cookie = document.cookie;
+  if (cookie.length > 8) {
+    document.getElementById("accoun").innerHTML = "Welcome back, " + cookie.substring(8) + "!";
+  }
+}
